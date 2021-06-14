139b46ae2ea407cc21840dd3a26ef405a56fd030
Santhosh Thomas <santhoshthomas@gmail.com>
2021-06-14 23:25:52 +0530

    spirited symphony

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from __future__ import division
 import time
 import os
 
-from flask import Flask, render_template,redirect
+from flask import Flask, render_template,redirect, Response
 
 from flask_restful import Api
 from flask_cors import CORS, cross_origin
@@ -242,3 +242,27 @@ def index():
 if __name__ == "__main__":
 
     app.run(host="0.0.0.0", debug=True, port=5000, threaded=True)
+
+
+
+import cv2
+camera = cv2.VideoCapture(0)  # use 0 for web camera
+#  for cctv camera use rtsp://username:password@ip_address:554/user=username_password='password'_channel=channel_number_stream=0.sdp' instead of camera
+# for local webcam use cv2.VideoCapture(0)
+
+def gen_frames():  # generate frame by frame from camera
+    while True:
+        # Capture frame-by-frame
+        success, frame = camera.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+
+@app.route('/video_feed')
+def video_feed():
+    #Video streaming route. Put this in the src attribute of an img tag
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
